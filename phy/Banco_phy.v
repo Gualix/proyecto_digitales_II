816f716b08d85ae6a3e39b0b120522485f3e480a
gualix <gualixgo@gmail.com>
2021-06-10 21:46:11 -0600

    banco_phy.v

--- phy/Banco_phy.v
+++ phy/Banco_phy.v
@@ -23,11 +23,16 @@ module BancoPruebas;
 	wire clk_4f;
 	wire clk_32f;
 
-    wire [7:0] In_0, In_1, In_2, In_3;                  //Entradas para el modulo transmisor
-    wire [7:0] res_In_0, res_In_1, res_In_2, res_In_3;  //Entradas para el modulo transmisor
-    wire validIn_0, validIn_1, validIn_2, validIn_3;    //Validadores de entrada
+    wire [7:0] In_0, In_1, In_2, In_3;                      //Entradas para el modulo transmisor
+    wire validIn_0, validIn_1, validIn_2, validIn_3;        //Validadores de entrada
+    
+    wire [7:0] res_In_0, res_In_1, res_In_2, res_In_3;      //Salidas del recirculadorpara el modulo transmisor
+    wire validRes_0, validRes_1, validRes_2, validRes_3;    //Validadores de salida del recirculador
 
-phy phy_general(
+    wire [7:0] Out_0, Out_1, Out_2, Out_3;                      //Salidas del receptor
+    wire validOut_0, validOut_1, validOut_2, validOut_3;    //Validadores de entrada
+
+phy phy_general(/*AUTOINST*/
 
     .reset(reset),
     .clk_f(clk_f),
@@ -35,34 +40,74 @@ phy phy_general(
     .clk_4f(clk_4f),
     .clk_32f(clk_32f),
 
-    .in0_tx(In_0),
+    .in0_tx(In_0),              // Se conectan las entradas al transmisor
     .in1_tx(In_1),
     .in2_tx(In_2),
     .in3_tx(In_3),
 
-    .valid_in0_tx(validIn_0),
+    .valid_in0_tx(validIn_0),   // Se conectan los valids de entradas al transmisor
     .valid_in1_tx(validIn_1),
     .valid_in2_tx(validIn_2),
     .valid_in3_tx(validIn_3),
 
+    .out0_tx(res_In_0),          // Se conectan las salidas del recirculador a las entradas
+    .out1_tx(res_In_1),
+    .out2_tx(res_In_2),
+    .out3_tx(res_In_3),
+
+    .valid_out0_tx(validRes_0),  // Se conectan los valids a las entradas
+    .valid_out1_tx(validRes_1),
+    .valid_out2_tx(validRes_2),
+    .valid_out3_tx(validRes_3),
+
+    .out0_rx(Out_0),            // Conexión de las salidas del receptor
+    .out1_rx(Out_1),
+    .out2_rx(Out_2),
+    .out3_rx(Out_3),
+
+    .valid_out0_rx(validOut_0), // Conexión de los valids de salidas del receptor
+    .valid_out1_rx(validOut_1),
+    .valid_out2_rx(validOut_2),
+    .valid_out3_rx(validOut_3),
+
 );
 
-probador_tx Probador(
+probador_tx Probador(/*AUTOINST*/
     .reset(reset),
     .clk_f(clk_f),
     .clk_2f(clk_2f),
     .clk_4f(clk_4f),
     .clk_32f(clk_32f),
 
-    .data_in0(In_0),
-    .data_in1(In_1),
-    .data_in2(In_2),
-    .data_in3(In_3),
+    .in0_tx(In_0),              // Se conectan las entradas al transmisor
+    .in1_tx(In_1),
+    .in2_tx(In_2),
+    .in3_tx(In_3),
 
-    .valid0(validIn_0),
-    .valid1(validIn_1),
-    .valid2(validIn_2),
-    .valid3(validIn_3),
+    .valid_in0_tx(validIn_0),   // Se conectan los valids de entradas al transmisor
+    .valid_in1_tx(validIn_1),
+    .valid_in2_tx(validIn_2),
+    .valid_in3_tx(validIn_3),
+
+    .out0_tx(res_In_0),          // Se conectan las salidas del recirculador a las entradas
+    .out1_tx(res_In_1),
+    .out2_tx(res_In_2),
+    .out3_tx(res_In_3),
+
+    .valid_out0_tx(validRes_0),  // Se conectan los valids a las entradas
+    .valid_out1_tx(validRes_1),
+    .valid_out2_tx(validRes_2),
+    .valid_out3_tx(validRes_3),
+
+    .out0_rx(Out_0),            // Conexión de las salidas del receptor
+    .out1_rx(Out_1),
+    .out2_rx(Out_2),
+    .out3_rx(Out_3),
+
+    .valid_out0_rx(validOut_0), // Conexión de los valids de salidas del receptor
+    .valid_out1_rx(validOut_1),
+    .valid_out2_rx(validOut_2),
+    .valid_out3_rx(validOut_3),
 
 );
 
